3716e1b1fffbdb75a85f071c7588148889d63859
MichelMiertschink <chereu.m@gmail.com>
2024-03-03 11:18:51 -0300

    Update seller and curtom service exception

--- WebSalesMvc/obj/Debug/net6.0/WebSalesMvc.AssemblyInfo.cs
+++ WebSalesMvc/obj/Debug/net6.0/WebSalesMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f63c143aa4541bc85ee8bb0a445d0b889b8f638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc264178739c4eefaeaa03dac4b970022320e451")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
