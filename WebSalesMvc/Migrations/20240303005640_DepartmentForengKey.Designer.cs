b2010f1ce3e4f4dc4666504bd583faf7782a95a9
MichelMiertschink <chereu.m@gmail.com>
2024-03-02 22:01:32 -0300

    referential integrity

--- WebSalesMvc/Migrations/20240303005640_DepartmentForengKey.Designer.cs
+++ WebSalesMvc/Migrations/20240303005640_DepartmentForengKey.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSalesMvc.Data;
 
@@ -10,9 +11,10 @@ using WebSalesMvc.Data;
 namespace WebSalesMvc.Migrations
 {
     [DbContext(typeof(WebSalesMvcContext))]
-    partial class WebSalesMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20240303005640_DepartmentForengKey")]
+    partial class DepartmentForengKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebSalesMvc/obj/Debug/net6.0/WebSalesMvc.AssemblyInfo.cs
+++ WebSalesMvc/obj/Debug/net6.0/WebSalesMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd7fc3ac8a2cf4bd80e367903260bb03153b7b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af85ae4c45213212443069fb3c9de993d202bb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSalesMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
